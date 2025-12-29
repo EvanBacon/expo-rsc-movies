9773acb1ddf7b9a0f294baa28dd82426a722cc31
evanbacon <bacon@expo.io>
2025-12-29 12:20:13 -0800

    update

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -1,12 +1,7 @@
 import { IconSymbol, IconSymbolName } from "@/components/ui/IconSymbol";
-import {
-  BottomTabBarButtonProps,
-  BottomTabNavigationOptions,
-} from "@react-navigation/bottom-tabs";
-import * as Haptics from "expo-haptics";
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
 import React from "react";
 // Better transitions on web, no changes on native.
-import { PlatformPressable } from "@react-navigation/elements";
 import { Tabs as NativeTabs } from "expo-router";
 import { Platform, useWindowDimensions } from "react-native";
 
@@ -90,18 +85,3 @@ Tabs.Screen = NativeTabs.Screen as React.FC<
     title?: string;
   }
 >;
-
-function HapticTab(props: BottomTabBarButtonProps) {
-  return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === "ios") {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
-  );
-}
